be422e9150388220a0d889a5a14c7a1e5f69afe4
kim-jingyu <swc05161@gmail.com>
2024-08-28 21:58:29 +0900

    [KAN-25] Feat: 비포애프터 콘텐츠 등록 UI 및 검색창 UI 구현

---
+++ app/src/main/java/com/innerpeace/themoonha/ui/fragment/beforeafter/BeforeAfterEnrollContentsPhraseFragment.kt
@@ -0,0 +1,30 @@
+package com.innerpeace.themoonha.ui.fragment.beforeafter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.innerpeace.themoonha.R
+
+class BeforeAfterEnrollContentsPhraseFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_before_after_enroll_contents_phrase,
+            container,
+            false
+        )
+    }
+}
