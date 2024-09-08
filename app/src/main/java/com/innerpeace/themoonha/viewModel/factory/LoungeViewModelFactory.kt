3f4fad0051ff88e582efc29a30cb31bd71c4c2cb
hodupie <hjchocmt@gmail.com>
2024-09-09 00:01:03 +0900

    [KAN-34] Feat: 주간 스케줄 보기 구현

--- app/src/main/java/com/innerpeace/themoonha/viewModel/factory/LoungeViewModelFactory.kt
+++ app/src/main/java/com/innerpeace/themoonha/viewModel/factory/LoungeViewModelFactory.kt
@@ -6,8 +6,9 @@ import com.innerpeace.themoonha.data.repository.LoungeRepository
 import com.innerpeace.themoonha.viewModel.LoungeViewModel
 
 class LoungeViewModelFactory(private val repository: LoungeRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoungeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
             return LoungeViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
